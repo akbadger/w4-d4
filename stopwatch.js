c925f45fe6ce636da71efa1620591cb16572fdb1
Aubrey Badger <akbadger@gmail.com>
2017-02-24 13:53:06 -0500

    added color to seconds and 15 second reset

--- stopwatch.js
+++ stopwatch.js
@@ -2,60 +2,86 @@ var minutes = 00;
 var seconds = 00;
 var totalTenths = 0;
 var startButton = document.querySelector('#start');
+var interval;
+var colorInterval;
+var timeoutInterval;
 
-document.querySelector('#start').addEventListener('click', function () {
-    setInterval(startTimer, 10)
+document.querySelector('#start').addEventListener('dblclick', function (e) {
+    var startButton = e.target;
+    interval = setInterval(startTimer, 10)
+    if (startButton.innerHTML === 'Resume') {
+        console.log('hello');
+        clearInterval(interval);
+        document.querySelector('#tenths').innerHTML = '00';
+        document.querySelector('#seconds').innerHTML = '00';
+        document.querySelector('#minutes').innerHTML = '00';
+        document.querySelector('#start').innerHTML = 'Start';
+    }
 });
+
+document.querySelector('#start').addEventListener('click', function (e) {
+    var startButton = e.target;
+
+    if (startButton.innerHTML === 'Start') {
+        startButton.innerHTML = 'Pause';
+        interval = setInterval(startTimer, 10)
+        colorInterval = setInterval(colorTimer, 1000);
+    }
+    else if (startButton.innerHTML === 'Pause') {
+        startButton.innerHTML = 'Resume';
+        clearInterval(interval);
+        clearInterval(colorInterval);
+        timeoutInterval = setInterval(timeout, 15000)
+    }
+    else if (startButton.innerHTML === 'Resume') {
+        startButton.innerHTML = 'Pause';
+        interval = setInterval(startTimer, 10)
+        colorInterval = setInterval(colorTimer, 1000);
+    }
+   
+});
+
+function changeColor() {
+    var red = Math.round(Math.random() * 255);
+    var green = Math.round(Math.random() * 255);
+    var blue = Math.round(Math.random() * 255);
+    document.querySelector('#seconds').style.color = `rgb(${red}, ${green}, ${blue})`
+}
+function colorTimer() {
+    changeColor()
+}
 
 function startTimer() {
     totalTenths++;
     var minutes = Math.floor(totalTenths / 6000);
     var seconds = Math.floor((totalTenths - minutes * 6000) / 100);
     var tenths = totalTenths - (minutes * 6000 + seconds * 100);
+    // changeColor();
 
-    if (tenths > 0) {
-        document.querySelector('#tenths').innerHTML = tenths;
-    }
-    if (tenths > 9) {
-        document.querySelector('#seconds').innerHTML = '0' + seconds;
-    }
-    if (seconds > 9) {
-        document.querySelector('#seconds').innerHTML = seconds;
-    }
-    if (tenths > 0) {
-        document.querySelector('#minutes').innerHTML = '0' + minutes;
-    }
-    if (minutes > 9) {
-        document.querySelector('#minutes').innerHTML = minutes;
-    }
+   if (tenths > 0) {
+       document.getElementById('tenths').innerHTML = '0' + tenths;
+   }
+   if (tenths > 9) {
+       document.getElementById('tenths').innerHTML = tenths;
+   }
+   if (tenths > 9) {
+       document.getElementById('seconds').innerHTML = '0' + seconds;
+   }
+   if (seconds > 9) {
+       document.getElementById('seconds').innerHTML = seconds;
+   }
+   if (tenths > 0) {
+       document.getElementById('minutes').innerHTML = '0' + minutes;
+       }
+   if (minutes > 9) {
+       document.getElementById('minutes').innerHTML = minutes;
+   }
 }
-
-function runningStartButton() {
-    // var startButton = document.querySelector('#start');
-    startButton.innerHTML = 'Pause';
-}
-document.querySelector('#start').addEventListener('click', runningStartButton);
-
-function pauseTimer() {
-    Interval = setInterval(startTimer, 10);
-    if (startButton.innerHTML === 'Pause') {
-        clearInterval(Interval);
+function timeout() {
+        clearInterval(interval);
+        document.querySelector('#tenths').innerHTML = '00';
+        document.querySelector('#seconds').innerHTML = '00';
+        document.querySelector('#minutes').innerHTML = '00';
+        document.querySelector('#start').innerHTML = 'Start';
     }
-    else {
-        runningStartButton();
-    }
-}
-startButton.addEventListener('click', pauseTimer);
 
-//    
-
-
-
-// function toggleResets() {}
-// if (startButton.innerText === 'Pause') {
-
-// } else if (startButton.innerText === 'Resume') {
-
-// }
-
-// }
